e3b6c59253e873fe77eb0190f0c441e121641a01
Hannes Maier-Flaig <hamfbohne@gmail.com>
2012-07-20 18:02:13 +0100

    + command line argument parsing, + dynamic mode

--- src/goinometer.c
+++ src/goinometer.c
@@ -15,17 +15,21 @@ Purpose of the setup is to let a stepper motor advance a certain amount of steps
 This (and some mechanical parts) form an automatic goinometer for an Brucker EPR spectrometer.
 
 ROADMAP:
-  + Record positions for every TTL pulse in sensible file format
+  o Record positions for every TTL pulse in sensible, simple file format
   + Add commandline parameters to set gear ratio, step size, …
-  + Use sensible folder structure and improve makefile
+  o Use sensible folder structure and improve makefile
   + Use time of TTL pulse to determine step width
-  + Regain platform independance (move away from ncurses)
-  + Add and LCD, pysical buttons for home and step size and implement a stand-alone solution (maybe…)
+  o Regain platform independance (move away from ncurses)
+  o Add and LCD, pysical buttons for home and step size and implement a stand-alone solution (maybe…)
+  o check for integer overflows on interrupts (unlikely to occur but who knows)
 */
 
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #include "ip_connection.h"
 #include "bricklet_io4.h"
@@ -39,11 +43,14 @@ ROADMAP:
 Stepper stepper;
 IO4 io;
 int nSteps = 0;
-int nStepsPerInterrupt = 10;
-double gearRatio = 2; // gear ratio > 1 means motor gear has less teeth
+int nStepsPerInterrupt; // default is set when parsing arguments
+float gear_ratio = 2; // gear ratio > 1 means motor gear has less teeth
+int dynamic_flag, record_flag;
+int last_value_mask;
+int last_interrupt_time_pin0 = 0;
 
 void print_stats() {
-    printw("\rNumber of steps performed: %3d (%6.2fdeg)", nSteps, ((float)nSteps)/gearRatio);
+    printw("\rNumber of steps performed: %3d (%6.2fdeg)", nSteps, ((float)nSteps)/gear_ratio);
     fflush(stdout);
 }
 
@@ -60,12 +67,12 @@ bool is_motor_ready() {
 }
 
 
-void advance() {
+void advance(int steps) {
     if( ! is_motor_ready())
       return;
       
-    stepper_set_steps(&stepper, nStepsPerInterrupt); // Drive 100 steps forward
-    nSteps += nStepsPerInterrupt;
+    stepper_set_steps(&stepper, steps); // Drive 100 steps forward
+    nSteps += steps;
     
     print_stats();
 }
@@ -75,7 +82,7 @@ void go_home() {
     if( ! is_motor_ready())
       return;
       
-    printw("\rGoing home from position %.2fdeg ...\n", ((float)nSteps)/gearRatio);
+    printw("\rGoing home from position %.2fdeg ...\n", ((float)nSteps)/gear_ratio);
     stepper_set_steps(&stepper, -nSteps);
     nSteps = 0;
     
@@ -84,7 +91,7 @@ void go_home() {
 
 
 void set_home() { 
-    printw("\rSet current position (%.2fdeg) as new home ...\n", ((float)nSteps)/gearRatio);
+    printw("\rSet current position (%.2fdeg) as new home ...\n", ((float)nSteps)/gear_ratio);
     nSteps = 0;
     
     print_stats();
@@ -92,17 +99,99 @@ void set_home() {
 
 
 void dispatch_interrupts(uint8_t interrupt_mask, uint8_t value_mask) {
+    int interrupt_time = time(NULL);
     if((1<<0) & interrupt_mask) {
         // interrupt on pin 0 -> advance if pin is high
-        if(interrupt_mask & value_mask == 1) {
-            advance();
+        if(interrupt_mask & value_mask == 1) {                
+            if( ! dynamic_flag) {
+                advance(nStepsPerInterrupt);
+            } else if(last_interrupt_time_pin0 != 0) {
+                advance((interrupt_time-last_interrupt_time_pin0)*gear_ratio/10.);
+            }
         }
+        last_interrupt_time_pin0 = interrupt_time;
     }
     // Add interrupt handling for home and step size button here.
+        
+    last_value_mask     = value_mask;
 }
 
-int main() {
-    char c;
+
+void display_usage() {
+    printf("This program drives a stepper motor when a TTL pulse is registered on port 0 of a tinkerforge IO4 bricklet.\n\n");
+    printf("Command line arguments:\n");
+    printf("    -a,  --angle       Angle by wich motor advances on interrupt (default: 5)\n");
+    printf("    -g,  --gear-ratio  Gear ratio between motor and sample rod (default: 2)\n");
+    printf("    -d,  --dynamic     Dynamic mode: --angle is ignored and instead TTL pulse length is used. 10ms = 0.1deg\n");
+    printf("    -r,  --record      Record the angular position after every interrupt\n");
+}
+
+
+void parse_arguments(int argc, char **argv) {
+    int c;
+    double avalue = 5; //default value for angle per interrupt
+    int option_index = 0;
+
+    while (1)
+      {
+       static struct option long_options[] =
+         {
+           {"dynamic",    no_argument, &dynamic_flag, 1},
+           {"record",     no_argument, &record_flag,  1},
+           {"angle",      required_argument, NULL, 'a'},
+           {"gear-ratio", required_argument, NULL, 'g'},
+           {NULL, 0, NULL, 0}
+         };
+
+       c = getopt_long (argc, argv, "dra:g:?h",
+                        long_options, &option_index);
+
+       if (c == -1)
+       {
+         break;
+       }
+
+       switch (c)
+         {
+         case 'a':
+           if ( ! strtod(optarg, NULL) )
+           {
+             printf ("option -a requires a double as value\n");
+             exit(1);
+           }
+           avalue = strtod(optarg, NULL); // nStepsPerInterrupt is set later to make sure gear_ratio is already set
+           break;
+         case 'g':
+           if ( ! strtod(optarg, NULL) )
+           {
+             printf ("option -g requires a double as value\n");
+             exit(1);
+           }
+           gear_ratio = strtod(optarg, NULL);
+           break;
+         case 'd':
+           dynamic_flag = 1;
+           break;
+         case 'r':
+           record_flag = 1;
+           break;
+         case 'h':
+         case '?':
+           display_usage();
+           exit(0);
+         default:
+           break;
+         }
+      }
+          
+    nStepsPerInterrupt = avalue * gear_ratio;
+}
+
+
+int main(int argc, char **argv) {
+    int c;
+    
+    parse_arguments(argc, argv);
    
     // Establish IP connection to brick deamon brickd
     IPConnection ipcon;
@@ -117,7 +206,7 @@ int main() {
     // Add IO4 to IP connection
     if(ipcon_add_device(&ipcon, &io) < 0) {
         fprintf(stderr, "Could not connect to IO4 bricklet\n");
-        exit(1);
+//        exit(1);
     }
     
     // Create stepper-device object
@@ -125,7 +214,7 @@ int main() {
     // Add device to IP connection
     if(ipcon_add_device(&ipcon, &stepper) < 0) {
         fprintf(stderr, "Could not connect to stepper brick\n");
-        exit(1);
+//        exit(1);
     }
 
     // Configure stepper driver
@@ -148,23 +237,31 @@ int main() {
     clear();
     noecho();
     
-    printw("Gear ratio is set to %d \n", gearRatio);
+    if(dynamic_flag) {
+        printw("Dynamic mode: The angle by which the motor advances corresponds to the TTL pulse length.\n");
+        printw(" (Thats the time pin 0 is high. 10ms = 0.1deg). The step size argument will be ignored.\n");
+    } else {
+        printw("Steps per interrupt %d (%.2fdeg).\n", nStepsPerInterrupt, (float)nStepsPerInterrupt/gear_ratio);
+    }
+    printw("Gear ratio is set to %f \n", gear_ratio);
     printw("Waiting for TTL interrupts.\n");
     printw(" Press h to go to the home position.\n Press H to set current position as home.\n Press a to advance.\n Press q to quit and display statistics\n");    
     
     while((c = getch()) != 'q') {
         if(c=='a')
-            advance();
+            advance(nStepsPerInterrupt);
         if(c=='h')  
             go_home();
         if(c=='H')  
             set_home();
     }
     
+    // Disconnect from brickd
     ipcon_destroy(&ipcon);
-    endwin();
     
-    printf("\rNumber of steps performed: %3d (%6.2fdeg)\n", nSteps, ((float)nSteps)/gearRatio);
+    endwin();
+        
+    printf("\rNumber of steps performed: %3d (%6.2fdeg)\n", nSteps, ((float)nSteps)/gear_ratio);
     fflush(stdout);
     return 0;
 }
